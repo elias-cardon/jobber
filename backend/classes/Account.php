a9cd4bbc33d4c747c0c5f4fa656a9475293a200b
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-07-11 17:19:05 +0200

    Starting our validation function

--- backend/classes/Account.php
+++ backend/classes/Account.php
@@ -2,6 +2,7 @@
 
 class Account{
     private $pdo;
+    private $errorArray=array();
 
     public function __construct()
     {
@@ -9,6 +10,12 @@ class Account{
     }
 
     public function register($fn,$ln,$un,$em,$pw,$pw2){
+        $this->validateFirstName($fn);
+    }
 
+    private function validateFirstName($fn){
+        if (strlen($fn) < 2 || strlen($fn) > 25){
+            array_push($this->errorArray,Constant::$firstNameCharacters);
+        }
     }
 }
---
+++ backend/classes/Constant.php
@@ -0,0 +1,5 @@
+<?php
+
+class Constant{
+    public static $firstNameCharacters = "Votre prénom doit contenir entre 2 et 25 caractères.";
+}
