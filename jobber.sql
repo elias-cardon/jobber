e457fc1c7389123c2f154300cdaf9494df6f667c
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-09-29 15:16:40 +0200

    Create jobber.sql

---
+++ jobber.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 29 sep. 2023 à 13:15
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `jobber`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profileImage` varchar(255) NOT NULL,
+  `profileCover` varchar(255) NOT NULL,
+  `following` int NOT NULL,
+  `followers` int NOT NULL,
+  `bio` text NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL,
+  `signUpDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `profileEdit` enum('0','1') NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstName`, `lastName`, `username`, `email`, `password`, `profileImage`, `profileCover`, `following`, `followers`, `bio`, `country`, `website`, `signUpDate`, `profileEdit`) VALUES
+(2, 'Elias', 'Cardon', '', 'elias.cardon.17@gmail.com', '$2y$10$D.A1tydYV.aVIELxJqOaaetvaBdXWy0fKs.iL2c7Hgqg7RfvzVNuO', './frontend/assets/images/profilePic.jpeg', './frontend/assets/images/backgroundCoverPic.svg', 0, 0, '', '', '', '2023-08-29 15:45:25', '0');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
