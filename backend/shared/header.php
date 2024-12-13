c5a0ce7036c7cab5b6c2a69a7170e797e0b594d8
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-12-13 14:10:53 +0100

    configure the header

--- backend/shared/header.php
+++ backend/shared/header.php
@@ -1,3 +1,8 @@
+<?php
+if (!isset($pageTitle)) {
+    $pageTitle = "Jobber. C'est ce qu'il se passe. | Jobber";
+}
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -5,7 +10,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jobber. C'est ce qu'il se passe. / Jobber</title>
+    <title><?php echo $pageTitle?></title>
+    <link rel="shortcut icon" href="./frontend/assets/favicon/twitter.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./frontend/assets/css/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="frontend/assets/css/style.css">
 </head>
 <body>
