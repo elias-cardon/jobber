01d32b70e77f72130774365191f6e6ed83ff418d
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-12-16 15:16:47 +0100

    Création de la structure du formulaire

--- sign.php
+++ sign.php
@@ -1,4 +1,5 @@
-<?php $pageTitle = 'Inscription | Jobber';0 ?>
+<?php $pageTitle = 'Inscription | Jobber';
+0 ?>
 <?php require_once './backend/shared/header.php'; ?>
 
 <section class="sign-container">
@@ -26,9 +27,37 @@
             <form action="sign.php" class="formField">
                 <div class="form-group">
                     <label for="firstName">Prénom</label>
-                    <input type="text" name="firstName" id="firstName">
+                    <input type="text" name="firstName" id="firstName" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Nom</label>
+                    <input type="text" name="lastName" id="lastName" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="pass">Mot de passe</label>
+                    <input type="password" name="pass" id="pass" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="cpass">Confirmer mot de passe</label>
+                    <input type="password" name="pass2" id="cpass" autocomplete="off">
+                </div>
+                <div class="s-password">
+                    <input type="checkbox" class="form-checkbox" id="s-password">
+                    <label for="s-password">Montrer le mot de passe</label>
+                </div>
+                <div class="form-btn-wrapper">
+                    <button type="submit" class="btn-form">S'inscrire</button>
+                    <input type="checkbox" id="check" name="remember">
+                    <label for="check">Se souvenir de moi</label>
                 </div>
             </form>
         </div>
+        <footer class="form-footer">
+            <p>Déjà inscrit ? <a href="login.php">Se connecter</a></p>
+        </footer>
     </div>
 </section>
