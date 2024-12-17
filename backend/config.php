e5b6b37c2e5dce821ced56e1c7827dd9193163a9
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-12-17 20:59:50 +0100

    Config in progress

--- backend/config.php
+++ backend/config.php
@@ -1,13 +1,23 @@
 <?php
+define("DB_HOST", "localhost");
+define("DB_NAME", "jobber");
+define("DB_USER", "jobba");
+define("DB_PASS", "password");
 
-$servername = "localhost";
-$username = "jobba";
-$password = "password"; // A changer au moment du déploiement
+$public_end = strpos($_SERVER['SCRIPT_NAME'],"/frontend")+8;
+$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=jobber", $username, $password);
-    //Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-}
+define("WWW_ROOT", $doc_root);
+//echo BASE_URL;
+
+//$servername = "localhost";
+//$username = "jobba";
+//$password = "password"; // A changer au moment du déploiement
+//
+//try {
+ //   $pdo = new PDO("mysql:host=$servername;dbname=jobber", $username, $password);
+//    //Set the PDO error mode to exception
+//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//} catch (PDOException $e){
+//    echo "Connection failed: " . $e->getMessage();
+//}
--- backend/initialize.php
+++ backend/initialize.php
@@ -2,3 +2,11 @@
 ob_start();
 
 date_default_timezone_set("Europe/Paris");
+
+require_once "config.php";
+
+spl_autoload_register(function ($class) {
+    require_once "classes/" . $class . ".php";
+});
+
+session_start();
